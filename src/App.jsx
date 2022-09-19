70b33e56b090e26bcb694c88c96f88f78a804c0d
Anri85 <aryasubagja20@gmail.com>
2022-09-19 11:09:47 +0700

    Pengenalan React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <h1>Bootcamp WGS Batch 3</h1>
+                <h3>Ramdhani Arya</h3>
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Nav from "./Nav";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* menambahkan tampilan nav pada fungsi render */}
+        <Nav />
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
